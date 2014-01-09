rocksclusters-attic/base

commit bfe712f16aa66f894bf2c5d9fee51cbb97ac9938

fix bug fix in MySQL-python 1.2.3 where sets are not anymore returned as python sets

src/command/commands/add/host/__init__.py
@@ -1,4 +1,4 @@
-# $Id: __init__.py,v 1.23 2011/07/23 02:30:25 phil Exp $
+# $Id: __init__.py,v 1.24 2012/03/09 01:44:12 clem Exp $
 #
 # @Copyright@
 # 
@@ -54,6 +54,10 @@
 # @Copyright@
 #
 # $Log: __init__.py,v $
+# Revision 1.24  2012/03/09 01:44:12  clem
+# fix bug fix in MySQL-python 1.2.3 where sets are not anymore returned as
+# python sets
+#
 # Revision 1.23  2011/07/23 02:30:25  phil
 # Viper Copyright
 #
@@ -262,7 +266,12 @@ class Command(command):
 		self.db.execute("""select a.os from appliances a,
 			memberships m where m.appliance = a.id and
 			m.name='%s'""" % (membership))
-		supported_os = list(self.db.fetchone()[0])
+
+		#bug fix in the MySQL libraries over sql sets
+		if rocks.version[0] == '6' :
+			supported_os = self.db.fetchone()[0]
+		else:
+			supported_os = list(self.db.fetchone()[0])
 
 		if osname is not None and osname not in supported_os:
 			self.abort("%s does not support %s" % (membership,

commit cb8d3bff5634927ec0c9a8b8c7ff6e0b723c0c35

commands: remove hack for bug in mysql-python when using sets
The fix to this bug was introduced in bfe712f16aa66f894bf2c5d9fee51cbb97ac9938
and it is now not necessary since we don't use anymore sets
and the os column is an enum

src/command/commands/add/host/__init__.py
@@ -273,24 +273,15 @@ class Command(command):
 			memberships m where m.appliance = a.id and
 			m.name='%s'""" % (membership))
 
-		supported_os = self.db.fetchone()
-
-		# bug fix in MySQL-python of centos 5.6 sql sets returns a set of Sets
-		# (Set(['linux']),) so we need to pop out the first containing set
-		# on rocks 6 it is just ('linux',) so no need to do this
-		import warnings
-		warnings.filterwarnings(action="ignore", message='the sets module is deprecated')
-		import sets
-		if len(supported_os) > 0 and isinstance(supported_os[0], sets.Set):
-			supported_os = list(supported_os[0])
+		supported_os, = self.db.fetchone()
 
 		if osname is not None and osname not in supported_os:
 			self.abort("%s does not support %s" % (membership,
 				osname))
 
 		if osname is None:
-			if len(supported_os) == 1:
-				osname = supported_os[0]
+			if supported_os:
+				osname = supported_os
 			else:
 				osname = 'linux'
 